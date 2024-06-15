f4fb87f3340ec7b4e4393a45f9356802d6fd1201
Nikolay Zmitrovich <zanzifm@mail.ru>
2024-06-15 12:35:56 +0700

    add model training

--- nn_module.py
+++ nn_module.py
@@ -1,3 +1,4 @@
+import torchvision
 import torch 
 from torch import nn
 import torch.nn.functional as F
@@ -15,32 +16,69 @@ class CosineComponent(nn.Module):
         W_norm = F.normalize(self.W, dim=0)
         return x_norm @ W_norm
 
-def arcface_loss(cosine, target, output_classes, m=.4):
+def arcface_loss(cosine, target, n_classes, m=.4):
         
     cosine = cosine.clip(-1+1e-7, 1-1e-7) 
     arcosine = cosine.arccos()
-    arcosine += F.one_hot(target, num_classes=output_classes) * m
+    arcosine += F.one_hot(target, num_classes=n_classes) * m
     cosine2 = arcosine.cos()
     
     return F.cross_entropy(cosine2, target)
 
     
 class DML(nn.Module):
-    def __init__(self, input_size, embedding_size, n_classes, dropout=0.1) -> None:
+    def __init__(self, embedding_size, n_classes, dropout=0.1) -> None:
         super(DML, self).__init__()
         
+        self.pretrain_resnet = torchvision.models.resnet18(pretrained=True)
+        input_embedding_size = self.pretrain_resnet.fc.out_features
+        self.pretrain_resnet = torch.nn.DataParallel(self.pretrain_resnet)
+        
         self.ffwd_model = nn.Sequential(
-            nn.Linear(input_size, 1024), 
+            nn.Linear(input_embedding_size, 1024), 
+            nn.BatchNorm1d(1024),
             nn.LeakyReLU(), 
             nn.Dropout(dropout), 
-            nn.Linear(1024, embedding_size)
+            nn.Linear(1024, 256), 
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(), 
+            nn.Dropout(dropout), 
+            nn.Linear(256, embedding_size)
         )
         
         self.last_layer = CosineComponent(embedding_size=embedding_size, n_classes=n_classes)
+        # self.last_layer = nn.Sequential(
+        #     nn.Linear(embedding_size, n_classes),
+        #     nn.Softmax(dim=1)
+        # )
+        
+    def get_pretrain_embeddings(self, batch):
+        with torch.no_grad():
+            return self.pretrain_resnet(batch)
         
     def get_embeddings(self, batch):
-        return self.ffwd_model(batch)
+        out = self.get_pretrain_embeddings(batch)
+        return self.ffwd_model(out)
 
     def forward(self, batch):
-        out = self.ffwd_model(batch)
+        out = self.get_embeddings(batch)
         return self.last_layer(out)
+    
+    def get_predict(self, data_loader, device):
+        self.eval()
+        self.to(device)
+        
+        predict = []
+        targets = []
+        with torch.no_grad():
+            for batch in data_loader:
+                predict.append(
+                    self(batch[0].to(device))
+                )
+                targets.append(batch[1])
+                
+        predict = torch.cat(predict)
+        predict /= predict.norm(p=2, dim=1)[:, None]
+        targets = torch.cat(targets)
+        
+        return predict, targets
